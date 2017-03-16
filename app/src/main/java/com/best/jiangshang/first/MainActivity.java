f8dd2739120583286e6847661e16e7f794548582
JiangShang123 <1603476665@qq.com>
2017-03-16 15:47:00 +0800

    将创建的类MyApplication在onCreate下建立链接

--- app/src/main/java/com/best/jiangshang/first/MainActivity.java
+++ app/src/main/java/com/best/jiangshang/first/MainActivity.java
@@ -6,25 +6,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
-import com.best.jiangshang.first.been.PicturesData;
-import com.best.jiangshang.first.been.Results;
-import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.StringCallback;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -33,72 +18,15 @@ public class MainActivity extends AppCompatActivity {
     ViewPager vPager;
     MyAdapter myadapter;
 
-    String url="http://gank.io/api/data/Android/10/1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyApplication myApplication=(MyApplication)getApplication();
         init();
         rb_weixin.setChecked(true);//刚开始进入第一个界面时设置为选中状态
-        //从网络上下载数据
-        OkHttpClient okHttpClient=new OkHttpClient.Builder()
-                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
-                .readTimeout(10000L,TimeUnit.MILLISECONDS)
-                .build();
-        OkHttpUtils.initClient(okHttpClient);
-        //String url="http://gank.io/api/data/Android/10/1";
-        OkHttpUtils.get().url(url).build().execute(new StringCallback() {
-            @Override
-            public void onError(Call call, Exception e, int id) {
 
-            }
 
-            @Override
-            public void onResponse(String response, int id) {
-           //  Log.d(TAG, response);
-               // Log.i(TAG, response);
-                PicturesData picturesData=null;
-                try {
-                    JSONObject jsonObject=new JSONObject(response);
-                    boolean error=jsonObject.getBoolean("error");
-                    JSONArray jsonArray=jsonObject.getJSONArray("results");
-                    List<Results> list=new ArrayList<Results>();
-                    List<String> imagesList=new ArrayList<String>();
-                    for(int i=0;i<jsonArray.length();i++){
-                        JSONObject jsonResults=(JSONObject)jsonArray.get(i);
-                        String _id=jsonResults.getString("_id");
-                        String createdAt=jsonResults.getString("createdAt");
-                        String desc=jsonResults.getString("desc");
-                       //String images=jsonResults.getString("images");
-
-                        if(jsonResults.has("images")){
-                            JSONArray imagesArray=jsonResults.getJSONArray("images");
-                            for(int k=0;k<imagesArray.length();k++){
-                                String images=(String) imagesArray.get(k);
-                                Log.d(TAG, "onResponse: "+images);
-                                imagesList.add(images);
-                            }
-                        }
-
-                        String publishedAt=jsonResults.getString("publishedAt");
-                        String source=jsonResults.getString("source");
-                        String type=jsonResults.getString("type");
-                        String url=jsonResults.getString("url");
-                        Boolean used=jsonResults.getBoolean("used");
-                        String who=jsonResults.getString("who");
-                        Results results=new Results(_id,createdAt,desc,imagesList,publishedAt,source,type,url,used,who);
-                        list.add(results);
-                    }
-                    Log.d(TAG,list.toString());
-                    picturesData = new PicturesData();
-                    picturesData.setError(error);
-                    picturesData.setResults(list);
-                    Log.d(TAG, "onResponse: " + picturesData.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     private void init() {
