99eef2dd03a0b5419b054a47771709fb56b73086
JiangShang123 <1603476665@qq.com>
2017-03-23 19:15:45 +0800

    显示webView

--- app/src/main/java/com/best/jiangshang/first/TXLFragment.java
+++ app/src/main/java/com/best/jiangshang/first/TXLFragment.java
@@ -1,18 +1,20 @@
 package com.best.jiangshang.first;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TXLFragment extends Fragment {
-
+    private WebView webView;
 
     public TXLFragment() {
         // Required empty public constructor
@@ -23,7 +25,12 @@ public class TXLFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_txl, container, false);
+        View view=inflater.inflate(R.layout.fragment_txl, container, false);
+        webView=(WebView)view.findViewById(R.id.tv_webView);
+        Intent intent = new Intent();
+        String url = intent.getStringExtra("url");
+        webView.loadUrl(url);
+        return view;
     }
 
 }
